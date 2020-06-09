4ce921339a50d2c17763b32649dedd87bfc01078
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-09 16:52:19 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/ColorThemedModules.pm
@@ -0,0 +1,31 @@
+package Acme::CPANModules::ColorThemedModules;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use strict;
+use Acme::CPANModulesUtil::Misc;
+
+our $LIST = {
+    summary => "Modules that has color theme support",
+    description => <<'_',
+
+**ColorTheme**
+
+Modules which support color themes and follow the <pm:ColorTheme> specification.
+
+<pm:JSON::Color>
+
+<pm:Text::ANSITable>
+
+**Others**
+
+_
+};
+
+Acme::CPANModulesUtil::Misc::populate_entries_from_module_links_in_description;
+
+1;
+# ABSTRACT:
